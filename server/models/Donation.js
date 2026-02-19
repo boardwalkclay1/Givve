b7d74386fbaf24ff6b4eb471674fa8e8770df8ca
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-02-19 15:03:09 -0500

    Update Donation.js

--- server/models/Donation.js
+++ server/models/Donation.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 const donationSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
   amount: Number,
   tier: Number,
-  paymentId: String, // PayPal payment ID
-  globalDonationIndex: Number, // 1,2,3...
+  paymentId: String,
+  globalDonationIndex: Number,
   createdAt: { type: Date, default: Date.now }
 });
 

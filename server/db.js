e7df0a05e2933380443169584e53daa18e8369f0
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-02-21 20:57:58 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -6,5 +6,7 @@ const { Pool } = pkg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false
 });

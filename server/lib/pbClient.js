04286e149837d47b3ed42ec5eaa89ea0f2bc1575
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-02-23 20:16:10 -0500

    Update pbClient.js

--- server/lib/pbClient.js
+++ server/lib/pbClient.js
@@ -1,4 +1,10 @@
-PS C:\Users\board\Downloads> railway shell
-Entering subshell with Railway variables available. Type 'exit' to exit.
+import PocketBase from "pocketbase";
 
-PS C:\Users\board\Downloads>
+const pb = new PocketBase(process.env.PB_URL);
+
+await pb.admins.authWithPassword(
+  process.env.PB_ADMIN_EMAIL,
+  process.env.PB_ADMIN_PASSWORD
+);
+
+export default pb;

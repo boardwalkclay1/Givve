ce354aefcbefd0d99ea871ee6d73c6f09a1ffabc
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-03-01 09:04:10 -0500

    Create db.js

---
+++ server/lib/db.js
@@ -0,0 +1,10 @@
+// server/lib/db.js
+import pkg from "pg";
+const { Pool } = pkg;
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+export default db;
